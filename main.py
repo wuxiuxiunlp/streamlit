c75061398028955745cdfb1c89002d5dab969d69
allenwu <allen.wu18621039969@gmail.com>
2022-11-15 11:07:48 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# @Time : 2022/11/15 11:06
+# @Author : allen.wu
+# @Email : allen.wu@deloitte.com
+# @File : main.py
+# @Project : streamlit
+
+import streamlit as st
+
+if __name__ == '__main__':
+    st.write("Hello World!")
